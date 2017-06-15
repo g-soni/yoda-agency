0f77242944ae7791af2719ac3597344fa3ebfdd0
Gaurav <gauravsoni.grep@gmail.com>
2017-06-15 23:53:19 +0530

    Added query for filters name & tag

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,3 +1,25 @@
 class Agency < ApplicationRecord
-  enum grade: [ :padawan, :jedi, :master ]
+  enum grade: [ 'Padawan', 'Jedi', 'Master' ]
+
+  has_many :tags
+  
+  default_scope { order(grade: :desc) }
+  
+  def self.search filter_params
+    if filter_params[:name].present? || filter_params[:tag].present?
+      joins(:tags).where("lower(agencies.name) = ? OR lower(tags.name) = ?", filter_params[:name].downcase, filter_params[:tag].downcase).uniq
+    else
+      self.all
+    end
+  end
+
+  def tag_list
+    tags.map(&:name).join(',')
+  end
+
+  def as_json(options = {})
+    super({:only => [:id, :name, :description, :grade]}.merge(methods: :tag_list))
+  end
+ 
 end
+
