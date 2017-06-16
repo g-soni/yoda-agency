b27746fc5feba27f688d7743c0b237943915ac82
Gaurav <gauravsoni.grep@gmail.com>
2017-06-16 12:15:13 +0530

    worked on test cases

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -5,7 +5,7 @@ class Agency < ApplicationRecord
   
   default_scope { order(grade: :desc) }
   
-  def self.search filter_params
+  def self.search filter_params = {}
     if filter_params[:name].present? || filter_params[:tag].present?
       joins(:tags).where("lower(agencies.name) = ? OR lower(tags.name) = ?", filter_params[:name].downcase, filter_params[:tag].downcase).uniq
     else
